b92c4d38c41826fbbe7a781dd4094469cf46c6d5
Freddie <fredjlawson@gmail.com>
2018-08-13 15:17:19 +0100

    added topup capability, passing tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,22 @@
-describe Oystercard do 
+require 'oystercard'
 
+describe Oystercard do 
+  describe "#balnce" do
+    it "returns the balance on the .oystercard" do
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  describe "#top-up" do
+    it "should increase the balance amount" do
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
+    end
+    it "should raise an error if maximum balance is exceeded by #topup" do
+      subject.top_up(90)
+      expect { subject.top_up(1) }.to raise_error("Maximum value on Oystercard reached!!")
+    end
+
+  end
 end
+
